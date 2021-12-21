421ba6417f3d845beb7f327596498574fe116f0c
Douglas Orr <douglas.orr99@gmail.com>
2021-12-21 17:32:44 +0000

    Basic Spotify API integration

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Uses the Spotify SDK to allow basic playback control using full-screen buttons & hardware keyboard.
 
+## Development
+
+ - Android Studio
+ - Download the Spotify Android SDK from https://github.com/spotify/android-sdk/releases to
+   `app/libs/`
+
 ## Links
 
  - Spotify Android SDK [home](https://developer.spotify.com/documentation/android/),
--- app/src/main/java/douglasorr/bigremote/HomeActivity.kt
+++ app/src/main/java/douglasorr/bigremote/HomeActivity.kt
@@ -2,15 +2,56 @@ package douglasorr.bigremote
 
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
+
 
 class HomeActivity : AppCompatActivity() {
+    private var spotifyConnection: SpotifyAppRemote? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
         findViewById<Button>(R.id.home_button_skip).setOnClickListener {
-            Log.d("DEBUG", "Click skip")
+            Log.d("User", "Skip")
+            val result = spotifyConnection?.playerApi?.skipNext()
+            if (result != null) {
+                result.setResultCallback {
+                    Log.d("Spotify", "Skip success")
+                }
+                result.setErrorCallback {
+                    Log.d("Spotify", "Skip error: ${it.message}")
+                }
+            }
         }
+
+        val connectionParams = ConnectionParams.Builder("358462dd3a7a4d92b910e67262c921b6")
+            .setRedirectUri("douglasorr.bigremote://callback")
+            .showAuthView(true)
+            .build()
+
+        Log.d("Spotify", "Connection params $connectionParams")
+
+        SpotifyAppRemote.connect(this, connectionParams,
+            object: Connector.ConnectionListener {
+                override fun onConnected(spotifyAppRemote: SpotifyAppRemote?) {
+                    Log.d("Spotify", "Connected: $spotifyAppRemote")
+                    spotifyConnection = spotifyAppRemote
+                }
+                override fun onFailure(error: Throwable?) {
+                    Log.d("Spotify", "Error - not connected: ${error?.message}")
+                }
+            }
+        )
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        Log.d("User", "onKeyDown $keyCode ${event?.unicodeChar}")
+        return super.onKeyDown(keyCode, event)
     }
 }

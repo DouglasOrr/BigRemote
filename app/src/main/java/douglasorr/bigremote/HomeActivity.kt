daa828080965c1bd75bd56b449809b82ecf46481
Douglas Orr <douglas.orr99@gmail.com>
2021-12-22 23:19:58 +0000

    Disconnect from Spotify when not in foreground (closes  (fixes #10)

--- app/src/main/java/douglasorr/bigremote/HomeActivity.kt
+++ app/src/main/java/douglasorr/bigremote/HomeActivity.kt
@@ -11,50 +11,51 @@ import com.spotify.android.appremote.api.SpotifyAppRemote
 
 
 class HomeActivity : AppCompatActivity() {
-    private var spotifyConnection: SpotifyAppRemote? = null
+    private var currentSpotifyConnection: SpotifyAppRemote? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
         findViewById<Button>(R.id.home_button_skip).setOnClickListener{ skip() }
+    }
+
+    private fun skip() {
+        Log.d("User", "Skip")
+        currentSpotifyConnection?.playerApi?.skipNext()?.apply {
+            setResultCallback { Log.d("Spotify", "Skip success") }
+            setErrorCallback { Log.d("Spotify", "Skip error ${it.message}") }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
 
         val connectionParams = ConnectionParams.Builder("358462dd3a7a4d92b910e67262c921b6")
             .setRedirectUri("douglasorr.bigremote://callback")
             .showAuthView(true)
             .build()
 
-        Log.d("Spotify", "Connection params $connectionParams")
-
         SpotifyAppRemote.connect(this, connectionParams,
             object: Connector.ConnectionListener {
                 override fun onConnected(spotifyAppRemote: SpotifyAppRemote?) {
                     Log.d("Spotify", "Connected: $spotifyAppRemote")
-                    spotifyConnection = spotifyAppRemote
+                    currentSpotifyConnection = spotifyAppRemote
                 }
                 override fun onFailure(error: Throwable?) {
                     Log.d("Spotify", "Error - not connected: ${error?.message}")
+                    currentSpotifyConnection = null
                 }
             }
         )
     }
 
-    private fun skip() {
-        Log.d("User", "Skip")
-        val result = spotifyConnection?.playerApi?.skipNext()
-        if (result != null) {
-            result.setResultCallback {
-                Log.d("Spotify", "Skip success")
-            }
-            result.setErrorCallback {
-                Log.d("Spotify", "Skip error: ${it.message}")
-            }
-        }
+    override fun onPause() {
+        super.onPause()
+        SpotifyAppRemote.disconnect(currentSpotifyConnection)
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         Log.d("User", "onKeyDown $keyCode ${event?.unicodeChar}")
-//        findViewById<Button>(R.id.home_button_skip).text = "Hard key $keyCode '${event?.unicodeChar}'"
 
         if (keyCode in 19..22) {
             skip()

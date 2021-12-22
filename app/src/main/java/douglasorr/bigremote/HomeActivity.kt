486f9d1b2c72fb001ca5e867266466f36843f45c
Douglas Orr <douglas.orr99@gmail.com>
2021-12-22 23:54:57 +0000

    Handle 'no Spotify app' (fixes #8)

--- app/src/main/java/douglasorr/bigremote/HomeActivity.kt
+++ app/src/main/java/douglasorr/bigremote/HomeActivity.kt
@@ -1,13 +1,18 @@
 package douglasorr.bigremote
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector
 import com.spotify.android.appremote.api.SpotifyAppRemote
+import com.spotify.android.appremote.api.error.CouldNotFindSpotifyApp
 
 
 class HomeActivity : AppCompatActivity() {
@@ -17,6 +22,11 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         findViewById<Button>(R.id.home_button_skip).setOnClickListener{ skip() }
+        findViewById<Button>(R.id.home_button_download).setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("https://play.google.com/store/apps/details?id=com.spotify.music")
+            })
+        }
     }
 
     private fun skip() {
@@ -38,12 +48,26 @@ class HomeActivity : AppCompatActivity() {
         SpotifyAppRemote.connect(this, connectionParams,
             object: Connector.ConnectionListener {
                 override fun onConnected(spotifyAppRemote: SpotifyAppRemote?) {
-                    Log.d("Spotify", "Connected: $spotifyAppRemote")
                     currentSpotifyConnection = spotifyAppRemote
+                    findViewById<View>(R.id.home_layout_main).visibility = View.VISIBLE
+                    findViewById<View>(R.id.home_button_download).visibility = View.GONE
+                    findViewById<View>(R.id.home_text_error).visibility = View.GONE
                 }
                 override fun onFailure(error: Throwable?) {
-                    Log.d("Spotify", "Error - not connected: ${error?.message}")
+                    Log.d("Spotify", "Error - not connected: ${error?.javaClass?.name} ${error?.message}")
                     currentSpotifyConnection = null
+                    if (error is CouldNotFindSpotifyApp) {
+                        findViewById<View>(R.id.home_layout_main).visibility = View.GONE
+                        findViewById<View>(R.id.home_button_download).visibility = View.VISIBLE
+                        findViewById<View>(R.id.home_text_error).visibility = View.GONE
+                    } else {
+                        findViewById<View>(R.id.home_layout_main).visibility = View.GONE
+                        findViewById<View>(R.id.home_button_download).visibility = View.GONE
+                        findViewById<TextView>(R.id.home_text_error).apply {
+                            visibility = View.VISIBLE
+                            text = "Spotify connection error: ${error?.javaClass?.simpleName}"
+                        }
+                    }
                 }
             }
         )
